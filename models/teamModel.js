f5adb0bc72c3a98fba37d6015e865bd6e3ba72f0
ttowntom <git.running054@passinbox.com>
2024-11-19 20:46:20 +0100

    adds team schema

--- models/teamModel.js
+++ models/teamModel.js
@@ -2,25 +2,82 @@ const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema(
 	{
-		name: {
+		userId: {
+			type: String,
+		},
+		teamName: {
 			type: String,
 			required: true,
+		},
+		teamIdUnity: {
+			type: String,
 		},
 		icon: {
 			type: String,
 			required: false,
 		},
-		teamPower: {
-			type: Number,
-			required: true,
-		},
 		inventory: {
-			type: Object,
-			required: true,
+			stars: {
+				type: { type: Number, default: 0 },
+			},
+			gold: {
+				type: { type: Number, default: 0 },
+			},
+			minerals: {
+				type: { type: Number, default: 0 },
+			},
+			wood: {
+				type: { type: Number, default: 0 },
+			},
+			weapons: {
+				weaponIdMongo: {
+					type: ObjectId,
+				},
+				weaponIdUnity: {
+					type: String,
+				},
+				weaponName: {
+					type: String,
+				},
+				ownerIdMongo: {
+					type: ObjectId,
+				},
+				ownerIdUnity: {
+					type: String,
+				},
+			},
+			armor: {
+				armorIdMongo: {
+					type: ObjectId,
+				},
+				armorIdUnity: {
+					type: String,
+				},
+				armorName: {
+					type: String,
+				},
+				ownerIdMongo: {
+					type: ObjectId,
+				},
+				ownerIdUnity: {
+					type: String,
+				},
+			},
+			cards: {
+				cardIdMongo: {
+					type: ObjectId,
+				},
+				cardIdUnity: {
+					type: String,
+				},
+				cardName: {
+					type: String,
+				},
+			},
 		},
-		character: {
-			type: Object,
-			required: true,
+		players: {
+			type: Array,
+			default: [],
 		},
 	},
 	{ timestamps: true }
